88ade56d55da90c1016ef3ad4c24559d9d6aaf5d
yeesan <yeesan2004@gmail.com>
2016-05-11 22:19:10 -0700

    Victoria made changes
    I put it into a JPanel, just to test out how it runs. I keep your original code, as comment. Just in case you need them back. If we don't, please delete it!

--- src/Controls.java
+++ src/Controls.java
@@ -1,4 +1,4 @@
-import java.awt.Color;
+/*import java.awt.Color;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -11,4 +11,26 @@ public class Controls extends JPanel{
 		setBackground(Color.black);
 		setSize(400,400);
 	}
+}*/
+
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class Controls extends JPanel{
+		// or JPanel, goes to the left side of whiteboard
+	public Controls()
+	{
+		setLayout(new GridLayout(5,1));
+	//	setBackground(Color.black);
+		setSize(400,400);
+		add(new JButton("hay u1"));//testing purpose
+		add(new JButton("hay u2"));//testing purpose
+		add(new JButton("hay u3"));//testing purpose
+		add(new JButton("hay u4"));//testing purpose
+		add(new JButton("hay u5"));//testing purpose
+	}
 }
+
