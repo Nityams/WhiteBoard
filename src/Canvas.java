40a8bbaa43084da10ec4a7e97971c817047548ac
Nityam Shrestha <nityamshrestha@gmail.com>
2016-05-11 20:13:32 -0700

    with add and remove

--- src/Canvas.java
+++ src/Canvas.java
@@ -1,10 +1,12 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import javax.swing.JPanel;
 
 public class Canvas extends JPanel{
-	
+	ArrayList<DShape> shapes;  // may need to use stack/queue to display last on top 
 	//list of shape objects and reflectes each after adding and editing..
 	//also defines the ordering of the shape as last shape comming first	
 	
@@ -16,4 +18,30 @@ public class Canvas extends JPanel{
 		
 	}
 	
+//called by buttons' action listener
+	/*
+	 * @param- selected shape to delete
+	 */
+	private void remove(DShape delshape)
+	{
+		Iterator<DShape> itr = shapes.iterator();
+		if(shapes.contains(delshape))
+		{
+			
+			while(itr.next() != delshape)
+			{
+				itr.next();
+			}
+			itr.remove();  // should remove the next element
+		}
+	}
+
+	/*
+	 *  @param- selected shape to add
+	 */
+	private void add(DShape delshape)
+	{
+		// may need to check instance of delshape
+		shapes.add(delshape);
+	}
 }
