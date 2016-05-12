708f93a51f6274331bec689bd15f348249b3e6b3
yeesan <yeesan2004@gmail.com>
2016-05-11 23:51:03 -0700

    no btnlistener yet

--- src/Controls.java
+++ src/Controls.java
@@ -1,36 +1,16 @@
-/*import java.awt.Color;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-public class Controls extends JPanel{
-		// or JPanel, goes to the left side of whiteboard
-	
-	public Controls()
-	{
-		setBackground(Color.black);
-		setSize(400,400);
+import java.awt.*;
+import javax.swing.*;
+public class Controls extends JPanel {
+	// or JPanel, goes to the left side of whiteboard
+	public Controls() {
+		setLayout(new GridLayout(5, 1));
+		setSize(400, 400);
+		add(new addPanel());// panel for creating shapes
+		add(new colorPanel());// panel for setting color
+		add(new textPanel());// panel for setting text
+		add(new posPanel());// panel for moving and removing
+		add(new tablePanel());// panel for the table, basically empty right now
 	}
-}*/
 
-import java.awt.Color;
-import java.awt.GridLayout;
-
-import javax.swing.JButton;
-import javax.swing.JPanel;
-
-public class Controls extends JPanel{
-		// or JPanel, goes to the left side of whiteboard
-	public Controls()
-	{
-		setLayout(new GridLayout(5,1));
-	//	setBackground(Color.black);
-		setSize(400,400);
-		add(new JButton("hay u1"));//testing purpose
-		add(new JButton("hay u2"));//testing purpose
-		add(new JButton("hay u3"));//testing purpose
-		add(new JButton("hay u4"));//testing purpose
-		add(new JButton("hay u5"));//testing purpose
-	}
 }
-
