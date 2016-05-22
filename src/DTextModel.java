03ffca01aaa9815d6c9bfc852e70cea2727cc513
yeesan <yeesan2004@gmail.com>
2016-05-22 01:17:18 -0700

    Update DTextModel.java

--- src/DTextModel.java
+++ src/DTextModel.java
@@ -1,4 +1,31 @@
+import java.awt.Color;
+import java.awt.Font;
 
-public class DTextModel {
+public class DTextModel extends DShapeModel {
+	String input;
+	Color color;
+	Font font;
 
+	public DTextModel(String input){
+		setInput(input);
+	}
+		
+	public void setInput(String input){
+		this.input = input;
+	}
+	public String getInput(){
+		return input;
+	}
+	public void setColor(Color color){
+		this.color = color;
+	}
+	public Color getColor(){
+		return color;
+	}
+	public void setFont(Font font){
+		this.font = font;
+	}
+	public Font getFont(){
+		return font;
+	}
 }
