338d03508a22fd51a9116cc36334d0fdb89812c6
yeesan <yeesan2004@gmail.com>
2016-05-22 01:17:02 -0700

    Update DText.java

--- src/DText.java
+++ src/DText.java
@@ -1,4 +1,34 @@
+import java.awt.*;
 
-public class DText extends DShape{
+public class DText extends DShape {
+	UI view;
+	DTextModel model ;
+	
+	public DText(String input)
+	{
+		model = new DTextModel(input);	
+		model.setInput(input);
+	}
+	public void draw(Graphics g){
+		Graphics2D graphic = (Graphics2D) g;
+		int x=100;
+		int y=100;
+		graphic.setColor(Color.black);
+		graphic.drawString(model.getInput(), x, y);
+	}
+
+	public void setColor(Color c){
+		model.setColor(c);
+	}
+	public Color getColor(){
+		return model.getColor();
+	}
+	public void setFont(Font font){
+		model.setFont(font);
+	}
+	public Font getFont(){
+		return model.getFont();
+	}
+
 
 }
